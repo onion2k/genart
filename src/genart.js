3677ffe947fab3cf796133e309571b3a78871b51
Chris Neale <chris@komododigital.co.uk>
2017-11-01 12:34:28 +0000

    Better color scheme

--- src/genart.js
+++ src/genart.js
@@ -22,7 +22,7 @@ var r = new Rune({
 let sq = [];
 
 let c = 24;
-let g = -20;
+let g = 15;
 let s = (primitives.size-(g*c)) / c;
 
 for (let i=0; i<(c*c); i++) {
@@ -36,20 +36,32 @@ for (let i=0; i<(c*c); i++) {
     
     group.vec = new Rune.Vector(x, y);
     group.move(x, y);
+
     // group.rotate(3*(i%7), x+s/2, y+s/2);
 
     let dist = group.vec.distance(primitives.center);
 
-    r.rect(0, 0, s, s, group).stroke('hsv', 0, 0, 100).fill('hsv', Rune.map(i, 0, c*c, 0, 256), 100, 100);
+    r.rect(0, 0, s, s, group)
+        .stroke(false)
+        .fill('hsv', 0, 0, Rune.map(dist, 0, primitives.size/1.3, 50, 100));
 
     sq.push(group);
 
 }
 
+let ts = performance.now();
+let delta = 0;
+let counter = 0;
+
 r.on('update', () => {
+
+    delta = performance.now() - ts;
+    ts = performance.now();
+
     sq.forEach((group) => {
         let dist = group.vec.distance(primitives.center);
         group.rotate(group.state.rotation+1+(Helpers.toPercent(dist,primitives.width)/25), group.state.x + (s/2), group.state.y + (s/2));
+        // group.move(Math.sin(counter++), 0, true);
     })
 });
 
