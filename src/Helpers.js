f27fb39d5778c5c957ba69a60989fe1fe1d46847
Chris Neale <chris@komododigital.co.uk>
2017-11-01 09:05:19 +0000

    V1 working nicely)

---
+++ src/Helpers.js
@@ -0,0 +1,8 @@
+
+class Helpers {
+    static toPercent(x,r) {
+        return (x/r)*100;
+    }
+}
+
+export default Helpers;
--- src/genart.js
+++ src/genart.js
@@ -1,33 +1,57 @@
 import Rune from 'rune.js';
 import genartCss from './genart.css';
+import Helpers from './Helpers';
+
+const primitives = {
+    width: document.body.clientWidth,
+    height: document.body.clientHeight,
+    widthHalf: document.body.clientWidth / 2,
+    heightHalf: document.body.clientHeight / 2,
+    center: new Rune.Vector(document.body.clientWidth/2, document.body.clientHeight/2),
+}
+
+primitives.size = primitives.height * 0.75;
+primitives.offset = { x: primitives.widthHalf - primitives.size / 2, y: primitives.heightHalf - primitives.size / 2 }
 
 var r = new Rune({
     container: "body",
-    width: 800,
-    height: 800
+    width: primitives.width,
+    height: primitives.height
 });
 
 let sq = [];
 
-let s = 30;
-let g = 16;
+let c = 24;
+let g = -20;
+let s = (primitives.size-(g*c)) / c;
 
-for (let x=0; x<256; x++) {
+for (let i=0; i<(c*c); i++) {
 
-    let t = x % 16;
-    let l = Math.floor(x / 16);
+    let t = i % c;
+    let l = Math.floor(i / c);
     
     var group = r.group();
-    group.move(20+t*(s+g), 20+l*(s+g));
-    group.rotate(2*(x%7), t*(s+g) + s/2, l*(s+g) + s/2);
-    var newrect = r.rect(0, 0, s, s, group).fill(x, x, 255);
+    let x = primitives.offset.x + t*(s+g);
+    let y = primitives.offset.y + l*(s+g);
+    
+    group.vec = new Rune.Vector(x, y);
+    group.move(x, y);
+    // group.rotate(3*(i%7), x+s/2, y+s/2);
+
+    let dist = group.vec.distance(primitives.center);
+
+    r.rect(0, 0, s, s, group).stroke('hsv', 0, 0, 100).fill('hsv', Rune.map(i, 0, c*c, 0, 256), 100, 100);
+
     sq.push(group);
+
 }
 
 r.on('update', () => {
     sq.forEach((group) => {
-        group.rotate(group.state.rotation+1, group.state.x + 10, group.state.y + 10);
+        let dist = group.vec.distance(primitives.center);
+        group.rotate(group.state.rotation+1+(Helpers.toPercent(dist,primitives.width)/25), group.state.x + (s/2), group.state.y + (s/2));
     })
 });
 
 r.play();    
+
