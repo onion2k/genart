7b6e1ea0143dcc2451034253035985a76a1f939e
Chris Neale <chris@komododigital.co.uk>
2017-12-07 12:49:28 +0000

    Twisty triangles

--- src/art/triangles.js
+++ src/art/triangles.js
@@ -3,7 +3,8 @@ import Helpers from '../Helpers';
 
 let ts = performance.now();
 let delta = 0;
-let counter = 0;
+let counter = 100;
+let d = 1;
 
 export default function(primitives, r) {
 
@@ -16,43 +17,46 @@ export default function(primitives, r) {
     
     let c = 30;
     let g = 8;
-    let size = 20;
+    let size = 40;
     let s = (primitives.size-(g*c)) / c;
 
     var base = r.group();
     
     for (let i=c; i>0; i--) {
     
-        let x1 = primitives.widthHalf + (Math.sin(60 * (Math.PI/180)) * size*i);
-        let y1 = primitives.heightHalf + (Math.cos(60 * (Math.PI/180)) * size*i);
+        var group = r.group(primitives.widthHalf,primitives.heightHalf,base);
 
-        let x2 = primitives.widthHalf + (Math.sin(180 * (Math.PI/180)) * size*i);
-        let y2 = primitives.heightHalf + (Math.cos(180 * (Math.PI/180)) * size*i);
+        let x1 = (Math.sin(60 * (Math.PI/180)) * size*i);
+        let y1 = (Math.cos(60 * (Math.PI/180)) * size*i);
 
-        let x3 = primitives.widthHalf + (Math.sin(300 * (Math.PI/180)) * size*i);
-        let y3 = primitives.heightHalf + (Math.cos(300 * (Math.PI/180)) * size*i);
+        let x2 = (Math.sin(180 * (Math.PI/180)) * size*i);
+        let y2 = (Math.cos(180 * (Math.PI/180)) * size*i);
+
+        let x3 = (Math.sin(300 * (Math.PI/180)) * size*i);
+        let y3 = (Math.cos(300 * (Math.PI/180)) * size*i);
         
-        r.triangle(x1,y1, x2,y2, x3,y3)
+        r.triangle(x1,y1, x2,y2, x3,y3, group)
             .fill('hsv', 0, 0, (i%2===0)?0:100);
     
-        // sq.push(group);
+        sq.push(group);
     
     }
 
     r.on('update', () => {
     
-        // counter++;
-        // delta = performance.now() - ts;
-        // ts = performance.now();
+        counter++;
+        delta = performance.now() - ts;
+        ts = performance.now();
 
-        // let f = Math.sin(counter/100);
+        let f = Math.sin(counter/100);
+
+        if (counter%200===0) { d = d * -1; }
     
-        // sq.forEach((group, i) => {
+        sq.forEach((r, i) => {
             
-        //     group.move((((i%c)-c/2) * f * 0.01), ((Math.floor(i/c)-c/2) * f * 0.01), true);
-        //     group.rotate(group.state.rotation+1, group.state.x + (s/2), group.state.y + (s/2));
+            r.rotate(r.state.rotation + (i / c) * 0.75 * d, r.state.x + (s/2), r.state.y + (s/2));
 
-        // });
+        });
     });
 
 }
