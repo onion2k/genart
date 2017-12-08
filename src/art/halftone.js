399b48884f77272eba53e1be93622d58e12115f0
Chris Neale <chris@komododigital.co.uk>
2017-12-08 08:43:46 +0000

    Halftone animation

--- src/art/halftone.js
+++ src/art/halftone.js
@@ -3,50 +3,56 @@ import Helpers from "../Helpers";
 
 let ts = performance.now();
 let delta = 0;
-let counter = 100;
-let d = 1;
+let counter = 0;
+let d = 0.99;
+let base;
 
 export default function(primitives, r) {
-  for (var x = r.stage.children.length - 1; x >= 0; x--) {
-    r.stage.remove(r.stage.children[x]);
-  }
-  r.off("update");
+    for (var x = r.stage.children.length - 1; x >= 0; x--) {
+        r.stage.remove(r.stage.children[x]);
+    }
+    r.off("update");
 
-  let sq = [];
-  var base = r.group();
+    base = r.group();
 
-  let s = 8;
-  let g = 8;
-  let perRow = Math.ceil(primitives.width / (s * 2 + g));
-  let n = perRow * perRow * 2;
+    let sq = [];
 
-  for (let x = 0; x < n; x++) {
-    let xPos =
-      s / 2 +
-      (x % perRow) * (s * 2 + g) +
-      (Math.floor(x / perRow) % 2 ? (s * 2 + g) / 2 : 0);
-    let yPos = s / 2 + Math.floor(x / perRow) * ((s * 2 + g) / 2);
+    let s = 15;
+    let g = 15;
+    let perRow = Math.ceil(primitives.width / (s * 2 + g));
+    let n = perRow * perRow * 2;
 
-    r.circle(xPos, yPos, s).fill("white");
-  }
+    for (let x = 0; x < n; x++) {
+        let xPos =
+        s / 2 +
+        (x % perRow) * (s * 2 + g) +
+        (Math.floor(x / perRow) % 2 ? (s * 2 + g) / 2 : 0);
+        let yPos = s / 2 + Math.floor(x / perRow) * ((s * 2 + g) / 2);
 
-  r.on("update", () => {
-    counter++;
-    delta = performance.now() - ts;
-    ts = performance.now();
+        var group = r.group(0,0,base);
+        
+        group.move(xPos, yPos);
 
-    // let f = Math.sin(counter / 100);
+        r.circle(0, 0, s, group).fill("white");
 
-    // if (counter % 200 === 0) {
-    //   d = d * -1;
-    // }
+        sq.push(group);
+        
+    }
 
-    // sq.forEach((r, i) => {
-    //   r.rotate(
-    //     r.state.rotation + i / c * 0.75 * d,
-    //     r.state.x + s / 2,
-    //     r.state.y + s / 2
-    //   );
-    // });
-  });
+    r.on("update", () => {
+        counter++;
+        delta = performance.now() - ts;
+        ts = performance.now();
+
+        let f = Math.sin(counter / 100);
+
+        if (counter % 50 === 0) {
+            d = (d>1) ? 0.99 : 1.01;
+        }
+
+        sq.forEach((r, i) => {
+            r.scale(d, 1);
+        });
+
+    });
 }
